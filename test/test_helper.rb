4e8210ed6b67b22bea83979147f23030b7f0fd8c
Zsolt Szalma <zsolt.szalma@gmail.com>
2022-06-13 21:20:06 +0200

    Finish static pages with tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,16 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+# require "rails/backtrace_cleaner"
+require "minitest/reporters"
+Minitest::Reporters.use!
+# Minitest.backtrace_filter = Rails.backtrace_cleaner
+
+# Minitest::Reporters.use!(
+#       Minitest::Reporters::DefaultReporter.new,
+#       ENV,
+#       Rails.backtrace_cleaner
+#     )
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
